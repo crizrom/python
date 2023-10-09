2c9931eadd3d17f8188905a885af0ff078f7ae0f
crizrom <99371631+crizrom@users.noreply.github.com>
2023-10-08 19:26:20 -0600

    basic string methods

---
+++ stringmethods.py
@@ -0,0 +1,11 @@
+name = "Sir Godzilla"
+name1 = "1"
+name2 = "ABC"
+
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name1.isdigit()) #numerical strings
+print(name2.isalpha()) #only letters
