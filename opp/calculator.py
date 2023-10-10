df89e4cf69d9789093f3564b42a0fae1ea8d21a0
crizrom <99371631+crizrom@users.noreply.github.com>
2023-10-10 15:45:26 -0600

    New oop files: calculator, test, snake(not finished yetgit status!

---
+++ opp/calculator.py
@@ -0,0 +1,19 @@
+class Calculator: 
+    def __init__(self, n1, n2):
+        self.sum = n1 + n2
+        self.minus = n1 - n2
+        self.division = n1 / n2
+        self.multiply = n1 * n2
+
+print('***********Welcome to the test calculator!!!***********')
+print('This is just a test, this calculator uses float numbers')
+
+num1 = float(input('Enter a number: '))
+num2 = float(input('Enter a second number: '))
+operation = Calculator(num1, num2)
+
+print(operation.sum)
+print(operation.minus)
+print(operation.division)
+print(operation.multiply)
+
---
+++ opp/test.py
@@ -0,0 +1,27 @@
+class Car:
+
+    def __init__(self,make,model,year,color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def drive(self):
+        print("This "+self.model+" is driving")
+
+    def stop(self):
+        print("This "+self.model+" is stopped")
+
+
+car_1 = Car("Chevy","Corvette",2021,"green")
+car_2 = Car("Ford","Ferrari",2022,"red")
+
+print(car_2.make)
+print(car_2.model)
+print(car_2.year)
+print(car_2.color)
+print(" ")
+
+
+car_1.drive()
+car_2.stop()
