6f655f54c24ca2f0898f9aea030a2122868d7406
crizrom <99371631+crizrom@users.noreply.github.com>
2023-10-11 00:27:33 -0600

    modified test file

--- opp/test.py
+++ opp/test.py
@@ -1,10 +1,11 @@
 class Car:
 
-    def __init__(self,make,model,year,color):
+    def __init__(self,make,model,year,color, pais):
         self.make = make
         self.model = model
         self.year = year
         self.color = color
+        self.pais = pais
 
     def drive(self):
         print("This "+self.model+" is driving")
@@ -13,13 +14,17 @@ class Car:
         print("This "+self.model+" is stopped")
 
 
-car_1 = Car("Chevy","Corvette",2021,"green")
-car_2 = Car("Ford","Ferrari",2022,"red")
+car_1 = Car("Chevy","Corvette",2021,"green", "Pala d'Oro")
+car_2 = Car("Ford","Ferrari",2022,"red", "plaza")
 
 print(car_2.make)
 print(car_2.model)
 print(car_2.year)
 print(car_1.color)
+setattr(car_1, "make", "Porsche")
+print(car_1.make)
+print(car_1.pais)
+print(car_2.pais)
 print(" ")
 
 
